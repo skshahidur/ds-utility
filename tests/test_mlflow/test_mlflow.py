3d50d9a296540a22f1a86d48838975634a017cb4
skshahidur <skshahidur@gmail.com>
2021-12-09 15:04:33 +0100

    skimage similarity

--- tests/test_mlflow/test_mlflow.py
+++ tests/test_mlflow/test_mlflow.py
@@ -1,9 +1,8 @@
 import os
 
 import numpy as np
-
-# import pytest
 from PIL import Image
+from skimage.metrics import structural_similarity as ssim
 
 from ds_utility.mlflow.sklearn_utils import classification_report_mlflow
 
@@ -45,31 +44,26 @@ def test_classification_report_mlflow():
         "c",
     ]
     path_to_asset = os.path.dirname(os.path.dirname(__file__)) + "/assets"
-    expected_image_dir = path_to_asset + "/observed/"
-    classification_report_mlflow(real, pred, path=expected_image_dir)
+    observed_image_dir = path_to_asset + "/observed/"
+    classification_report_mlflow(real, pred, path=observed_image_dir)
 
-    observed_image_path = path_to_asset + "/expected/classification_report.png"
+    expected_image_path = path_to_asset + "/expected/classification_report.png"
     assert_images_equal(
-        expected_image_dir + "classification_report.png", observed_image_path
+        observed_image_dir + "classification_report.png", expected_image_path
     )
 
 
 def assert_images_equal(image_1: str, image_2: str):
-    img1 = Image.open(image_1)
-    img2 = Image.open(image_2)
 
-    # Convert to same mode and size for comparison
-    img2 = img2.convert(img1.mode)
-    img2 = img2.resize(img1.size)
-
-    print(np.asarray(img1).shape)  # noqa
-    print(np.asarray(img2).shape)  # noqa
-
-    sum_sq_diff = np.sum(
-        (np.asarray(img1).astype("float") - np.asarray(img2).astype("float")) ** 2
+    img1 = np.asfarray(Image.open(image_1).convert("L"))
+    img2 = np.asfarray(Image.open(image_2).convert("L"))
+    similiarity = ssim(
+        img1,
+        img2,
+        multichannel=True,
+        gaussian_weights=True,
+        sigma=1.5,
+        use_sample_covariance=False,
+        data_range=255,
     )
-
-    if sum_sq_diff == 0:
-        pass
-    else:
-        assert 1 / np.sqrt(sum_sq_diff) < 0.001
+    assert similiarity > 0.9
